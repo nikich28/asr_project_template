ad83c172661606fdcabc1979848fe0cdb1f08fa7
Nikita Andreev <nikita01n01@gmail.com>
2021-10-23 19:22:37 +0300

    add augs

--- hw_asr/augmentations/wave_augmentations/augmentations.py
+++ hw_asr/augmentations/wave_augmentations/augmentations.py
@@ -1,14 +1,15 @@
 import torch_audiomentations
+import random
 from torch import Tensor, rand, randint
-from torchaudio.transforms import Fade, Vol
+from torchaudio import transforms
 
 from hw_asr.augmentations.base import AugmentationBase
 
 
 class Vol(AugmentationBase):
     def __init__(self, *args, **kwargs):
-        gain = rand((1,)).item()
-        self.aug = Vol(gain)
+        gain = random.random()
+        self.aug = transforms.Vol(gain)
 
     def __call__(self, wave):
         return self.aug(wave)
@@ -18,7 +19,7 @@ class Fade(AugmentationBase):
     def __init__(self, *args, **kwargs):
         f = 20
         fade = randint(0, f, size=(1, )).item()
-        self.aug = Fade(fade, f - fade)
+        self.aug = transforms.Fade(fade, f - fade)
 
     def __call__(self, wave):
         return self.aug(wave)
