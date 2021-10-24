a33eeb09668ac6feec894a5466f57660db8051e2
Nikita Andreev <nikita01n01@gmail.com>
2021-10-25 00:53:45 +0300

    add model

--- hw_asr/augmentations/spectrogram_augmentations/augmentations.py
+++ hw_asr/augmentations/spectrogram_augmentations/augmentations.py
@@ -1,5 +1,6 @@
 from torchaudio import transforms
 from torch import randn, Tensor
+import random
 
 from hw_asr.augmentations.base import AugmentationBase
 
@@ -7,17 +8,25 @@ from hw_asr.augmentations.base import AugmentationBase
 class FrequencyMasking(AugmentationBase):
     def __init__(self, frequency=15, *args, **kwargs):
         self.augm = transforms.FrequencyMasking(frequency)
+        self.p = 0.5
 
     def __call__(self, data: Tensor):
-        return self.augm(data)
+        if random.random() < self.p:
+            x = data.unsqueeze(1)
+            return self.augm(x).squeeze(1)
+        return data
 
 
 class TimeMasking(AugmentationBase):
     def __init__(self, frequency=35, *args, **kwargs):
         self.augm = transforms.TimeMasking(frequency)
+        self.p = 0.5
 
     def __call__(self, data: Tensor):
-        return self.augm(data)
+        if random.random() < self.p:
+            x = data.unsqueeze(1)
+            return self.augm(x).squeeze(1)
+        return data
 
 
 class Normalize(AugmentationBase):
